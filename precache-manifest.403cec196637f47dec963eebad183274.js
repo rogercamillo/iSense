f9e0018a7144099fed0e7ffe394c9f0469af8be0
allcantara <brunoalcantarajc@gmail.com>
2020-05-25 11:43:00 -0300

    Updates

---
+++ precache-manifest.403cec196637f47dec963eebad183274.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867826c3fd50499cd0e09ab52f91c61c",
+    "url": "/iSense/index.html"
+  },
+  {
+    "revision": "cf216548b18bce162e79",
+    "url": "/iSense/static/css/main.4a52cbcd.chunk.css"
+  },
+  {
+    "revision": "7b0e91dcd8f0a9539123",
+    "url": "/iSense/static/js/2.b68b6a4c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/iSense/static/js/2.b68b6a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf216548b18bce162e79",
+    "url": "/iSense/static/js/main.038b6f39.chunk.js"
+  },
+  {
+    "revision": "6728ba009b910901181f",
+    "url": "/iSense/static/js/runtime-main.c1326b1d.js"
+  },
+  {
+    "revision": "0a5b5c55b73f577ff4ac8c9c31b4c183",
+    "url": "/iSense/static/media/aparaj.0a5b5c55.ttf"
+  },
+  {
+    "revision": "861d4bdd60a62e0c2cec0a6e91769d43",
+    "url": "/iSense/static/media/audio.861d4bdd.mp3"
+  },
+  {
+    "revision": "e029cff5f1a975adac2e8768efe47115",
+    "url": "/iSense/static/media/bg.e029cff5.jpg"
+  },
+  {
+    "revision": "cec4d8e70fd0581e461dd509f234e37c",
+    "url": "/iSense/static/media/gif1.cec4d8e7.gif"
+  },
+  {
+    "revision": "fef9453ab699047cfe56de7a009545c6",
+    "url": "/iSense/static/media/login.fef9453a.gif"
+  },
+  {
+    "revision": "6224ca361ecbfc72732acbe9959e4a0c",
+    "url": "/iSense/static/media/logo.6224ca36.png"
+  },
+  {
+    "revision": "b1644e5d0626f218fa8a172413b542cb",
+    "url": "/iSense/static/media/spotify.b1644e5d.png"
+  },
+  {
+    "revision": "52820f2b44eb8beb510e9a2039e1b287",
+    "url": "/iSense/static/media/video.52820f2b.mp4"
+  }
+]);
